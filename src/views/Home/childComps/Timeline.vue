17e4b3d898cb45ffc8f0f2563a45d6db13604db0
Tsing09073 <1541957865@qq.com>
2023-09-04 20:45:43 +0800

    first commit

---
+++ src/views/Home/childComps/Timeline.vue
@@ -0,0 +1,63 @@
+<template>
+  <span class="title">摆烂：进厂之路就在脚下</span>
+  <el-timeline>
+    <el-timeline-item
+      v-for="(activity, index) in activities"
+      :key="index"
+      :timestamp="activity.timestamp"
+      :color="activity.color"
+    >
+      {{ activity.content }}
+    </el-timeline-item>
+  </el-timeline>
+</template>
+
+<script setup>
+const activities = [
+  {
+    content:
+      "进厂深造",
+    timestamp: "2023-09-08",
+    color: "#eebe77",
+  },
+  {
+    content:
+      "寝室摆烂",
+    timestamp: "2022-05-03",
+  },
+  {
+    content:
+      "持续性摆烂",
+    timestamp: "2021-11-02",
+  },
+  
+  {
+    content:
+      "间接性图强",
+    timestamp: "2021-10-16",
+  },
+  {
+    content:
+      "半途而废",
+    timestamp: "2021-09-16",
+  },
+  {
+    content:
+      "参加比赛",
+    timestamp: "2021-03-09",
+  },
+  {
+    content:
+      "灰常勤奋",
+    timestamp: "2020-11-02",
+  },
+  {
+    content:
+      "初入大学''",
+    timestamp: "2020-09-18",
+  },
+];
+</script>
+
+<style>
+</style>
---
+++ src/views/Map/Map.vue
@@ -0,0 +1,32 @@
+
+<template>
+  <baidu-map
+    class="admin-map"
+    ak="需要申请换成自己的ak"
+    v="3.0"
+    type="WebGL"
+    center="北京"
+    :scroll-wheel-zoom="true"
+    开启鼠标滚动放大
+    @ready="mapReady"
+  />
+</template>
+
+<script setup>
+import { BaiduMap } from "vue-baidu-map-3x";
+const mapReady = ({ BMap, map }) => {
+  map.setMapStyleV2({
+    styleId: "7fbf433b917f6ca463cb73378a6bb226",
+    //设置地图自定义风格，百度api上也有说明
+  });
+};
+</script>
+
+<style>
+.admin-map {
+  width: 100%;
+  height: 450px;
+}
+</style> 
+
+
